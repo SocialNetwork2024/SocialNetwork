62f8773dff1045e504909cd29bb8a98410910edf
jabril205 <jabrilslim@gmail.com>
2024-02-11 16:45:25 +0100

    css moreeeeeeeee style

--- src/components/Sections/Middle.jsx
+++ src/components/Sections/Middle.jsx
@@ -1,110 +1,142 @@
-import {useState} from 'react'
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline'
-import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
-import Button from '@mui/material/Button';
-import axios from 'axios'
+import { useState } from "react";
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import axios from "axios";
+import "./middle.css";
 
+const Middle = ({ posts }) => {
+  const [body, setBody] = useState("");
+  const [file, setFile] = useState(null);
+  const [content, setContent] = useState("");
 
+  const handleCreate = (obj) => {
+    axios
+      .post("http://localhost:3000/post/add", obj)
+      .then((res) => {
+        console.log("created");
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  };
+  const HandleComment = (id, content) => {
+    axios
+      .post(`http://localhost:3000/comment/add/${id}`, content)
+      .then((res) => {
+        console.log("comment added");
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  };
 
-const Middle =({posts})=> {
-  const[body,setBody]=useState('')
-  const[file,setFile]=useState(null)
-  const[content,setContent]=useState('')
+  const handleDelete = (id) => {
+    axios
+      .delete(`http://localhost:3000/post/delete/${id}`)
+      .then((res) => {
+        console.log("deleted");
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  };
 
+  return (
+    <div className="add">
+      <Box>
+        <h3>Feed</h3>
+        <textarea
+          placeholder="New Post"
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        ></textarea>
+        <input
+          type="file"
+          onChange={(e) => {
+            setFile(e.target.files[0]);
+            console.log(e.target.files[0]);
+          }}
+        />
+        <button
+          onClick={() => {
+            handleCreate({
+              body: body,
+              file: file,
+            });
+          }}
+        >
+          Upload
+        </button>
 
-const handleCreate=(obj)=>{
-  axios.post('http://localhost:3000/post/add',obj)
-  .then((res)=>{console.log("created")})
-  .catch((error)=>{console.log("error")})
-}
-const HandleComment=(id,content)=>{
-  axios.post(`http://localhost:3000/comment/add/${id}`,content)
-  .then((res)=>{console.log("comment added")})
-  .catch((error)=>{console.log("error")})
+        {posts.map((post, index) => {
+          return (
+            <div className="posts" key={index}>
+              {post.body}
+              <img
+                src={post.file}
+                alt="postimage"
+                style={{ width: "360px", height: "240px" }}
+              />
+              <p style={{ fontSize: "10px", textAlign: "right" }}>
+                {post.createdAt}
+              </p>
+              <textarea
+                name="New Post"
+                id=""
+                cols="30"
+                rows="10"
+                placeholder="New Comment"
+                style={{
+                  height: "40px",
+                  width: "500px",
+                  marginLeft: "20px",
+                }}
+                onChange={(e) => {
+                  setContent(e.target.value);
+                }}
+              ></textarea>
+              <button
+                onClick={(id) => {
+                  HandleComment(post.id, {
+                    content: content,
+                  });
+                  window.location.reload();
+                }}
+              >
+                add a Comment
+              </button>
+              <p>
+                {post.comments.map((comment) => {
+                  return <div>{comment.content}</div>;
+                })}
+              </p>
 
-}
+              <button
+                onClick={() => {
+                  handleDelete(post.id);
+                  window.location.reload();
+                }}
+              >
+                {" "}
+                Delete{" "}
+              </button>
+            </div>
+          );
+        })}
+      </Box>
 
-const handleDelete=(id)=>{
-  axios.delete(`http://localhost:3000/post/delete/${id}`)
-  .then((res)=>{console.log("deleted")})
-    .catch((error)=>{console.log("error")})
-}
-
-
-    return (
-      <>
-<React.Fragment>
-      <CssBaseline />
-  <Container maxWidth="sm">
-   <Box sx={{ bgcolor: '#cfe8fc' }}>
-        <h3 style={{"textAlign":"center"}}>Feed</h3>
-        <textarea name="New Post"  id="" cols="30" rows="10" placeholder="New Post" style={{"height":"100px","width":"500px","marginLeft":"20px"}}
-        onChange={(e)=>{setBody(e.target.value)}}></textarea>
-        <input type="file"  onChange={(e)=>{setFile(e.target.files[0]);console.log(e.target.files[0])}}/>
-          <Button onClick={()=>{
-        handleCreate({
-            body:body,
-            file:file
-        })
-      }}>Upload</Button>
-
-                  {posts.map((post,index)=>{
-                    return (
-                      <div className='posts' key={index}>
-                      {post.body}
-                      <img src={post.file} alt="postimage"  style={{"width":"360px","height":"240px"}} />
-                      <p style={{"fontSize":"10px","textAlign":"right"}}>{post.createdAt}</p>
-                      <textarea name="New Post"  id="" cols="30" rows="10" placeholder="New Comment" style={{"height":"40px","width":"500px","marginLeft":"20px"}}
-        onChange={(e)=>{setContent(e.target.value)}}></textarea>
-        <button onClick={(id)=>{HandleComment(post.id,
-          {
-            content:content
-          }
-        );window.location.reload()
-          
-        }}>add a Comment</button>
-                      <p>{post.comments.map((comment)=>{
-                        return (
-                          <div>
-                            {comment.content}
-                          </div>
-                        )
-                      })}</p>
-                    
-                      <button onClick={()=>{handleDelete(post.id);window.location.reload()}}> Delete </button> 
-                      </div>
-                    )
-                  })}
-
-
-
-    </Box>
-  </Container>
-</React.Fragment>
-
-    {/* <input type="text" onChange={(e)=>{setBody(e.target.value)}}/>
+      {/* <input type="text" onChange={(e)=>{setBody(e.target.value)}}/>
 <input type="file" onChange={(e)=>{setFile(e.target.value)}}/>
 <button onClick={(id)=>{handleUpdate(post.id,{
     body:body,
     file:file
 });window.location.reload()}}> Update  </button>*/}
+    </div>
+  );
+};
 
-
-
-
-
-
-      </>
-     
-    )
-}
-
-export default Middle
-
-
-            
-          
-        
-   
+export default Middle;
--- src/components/Sections/Navbar.jsx
+++ src/components/Sections/Navbar.jsx
@@ -5,11 +5,11 @@ const NavBar = () => {
     <nav className="navbar" >
       <ul>
         <li><a href="/home">Home</a></li>
-        <li className="search-container"><input type="text" />
-        <button>Search</button></li>
+        <li><a href="/login">Login</a></li>
         <li><a href="/profil" placeholder="Search">Profil</a></li>
         <li><img src="https://media.istockphoto.com/id/1388253782/photo/positive-successful-millennial-business-professional-man-head-shot-portrait.jpg?s=1024x1024&w=is&k=20&c=v0FzN5RD19wlMvrkpUE6QKHaFTt5rlDSqoUV1vrFbN4=" alt="propict" className='propic' /></li>
-        <li><a href="/login">Login</a></li>
+        <li className="search-container"><input type="text" />
+        <button>Search</button></li>
       </ul>
     </nav>
   )
